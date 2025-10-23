da6bf09fe9261e0e7ec40ad1ba0d433b7687d668
TUM-08 <arthur.nsp.tum@gmail.com>
2025-10-23 08:56:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+   <div>
+
+    <h1>LOGIN</h1>
+  usuario:
+    <input type="usuario" />
+  senha:
+    <input type="senha" />
+    <checkbox>
+      <a href="http://"></a>
+      </checkbox>
+      <a href="http://"></a><button></button>
+      <button>Entrar</button>
+      <p>logar com</p>
+      <div><img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" /><small>
+        </small></div>
+    
+
+
+   </div>
+
+      </div>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
